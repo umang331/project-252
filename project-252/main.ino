88932b3c2896cc3c769a012e3429aff7cc30b0e0
Umang balicha <54236975+umang331@users.noreply.github.com>
2023-05-09 13:44:29 +0530

    main.ino

--- project-252/main.ino
+++ project-252/main.ino
@@ -1,7 +1,7 @@
 #include<WiFi.h>
 #include<HTTPClient.h>
 
-const char ssid[] = "ssid";
+const char ssid[] = "Tenda_C106E";
 const char password[] = "password";
 
 String api = "http://192.168.1.32:5000/add";
